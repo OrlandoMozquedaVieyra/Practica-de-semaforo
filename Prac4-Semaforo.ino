89bf4e383240f3e0622cbe7b99a9ca82e3ba2dde
OrlandoFelipeMozquedaVieyra <orlandomozqueda1993@live.com>
2018-02-19 10:41:35 -0600

    codigo y fotos del programa semafoto
    Programa de arduino haciendo un semaforo
    
    Co-Authored-By: orlandomozquedavieyra <orlandomozquedavieyra@users.noreply.github.com>

---
+++ Prac4-Semaforo.ino
@@ -0,0 +1,73 @@
+// Project 4 - Interactive Traffic Lights
+//Este codigo desarrollado en arduino. Para realizar la practica de un semaforo con un pulsador y un potenciometro.
+//En el codigo se demuestra que realiza cada linea de codigo para comprender bien su funcion en el programa.
+int carRed = 12; // assign the car lights
+int carYellow = 11;
+int carGreen = 10;
+int pedRed = 9; // assign the pedestrian lights
+int pedGreen = 8;
+int button = 2; // button pin
+int crossTime = 5000; // time allowed to cross
+unsigned long changeTime; // time since button pressed
+void setup() {
+ pinMode(carRed, OUTPUT);
+ pinMode(carYellow, OUTPUT);
+ pinMode(carGreen, OUTPUT);
+ pinMode(pedRed, OUTPUT);
+ pinMode(pedGreen, OUTPUT);
+ pinMode(button, INPUT); // button on pin 2
+ // turn on the green light
+ digitalWrite(carGreen, HIGH);
+ digitalWrite(pedRed, HIGH);
+ Serial.begin(9600);//para hacer la comunicacion del serial
+}
+void loop() {
+ //Guardar en una variable entera el valor del potenciómetro 0 a 1024   
+ float valor = analogRead(A0);    
+ //Imprime en la consola serial el valor de la variable  
+ float tiempo = (valor * 1000)-3000;
+ Serial.print("tiempo de espera es: ");
+ Serial.println(tiempo);   
+ delay(300);
+ int state = digitalRead(button);
+ /* check if button is pressed and it is
+ over 5 seconds since last button press */
+ if (state == HIGH && (millis() - changeTime) > tiempo) {
+ // Call the function to change the lights
+ changeLights();
+ }
+}
+void changeLights() {
+ digitalWrite(carGreen, LOW); // green off
+ digitalWrite(carYellow, HIGH); // yellow on
+ delay(2000); // wait 2 seconds
+
+ digitalWrite(carYellow, LOW); // yellow off
+ digitalWrite(carRed, HIGH); // red on
+ delay(1000); // wait 1 second till its safe
+
+ digitalWrite(pedRed, LOW); // ped red off
+ digitalWrite(pedGreen, HIGH); // ped green on
+ delay(crossTime); // wait for preset time period
+
+ // flash the ped green
+ for (int x=0; x<10; x++) {
+ digitalWrite(pedGreen, HIGH);
+ delay(250);
+ digitalWrite(pedGreen, LOW);
+ delay(250);
+ }
+ // turn ped red on
+ digitalWrite(pedRed, HIGH);
+ delay(500);
+
+ digitalWrite(carYellow, HIGH); // yellow on
+ digitalWrite(carRed, LOW); // red off
+ delay(1000);
+ digitalWrite(carGreen, HIGH);
+ digitalWrite(carYellow, LOW); // yellow off
+
+ // record the time since last change of lights
+ changeTime = millis();
+ // then return to the main program loop
+}
